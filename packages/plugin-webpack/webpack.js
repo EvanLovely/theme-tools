0f6503f970919fd98d34536333a8b6ba2e230b6f
Evan Lovely <Evan@eBook.local>
2017-10-26 11:20:24 -0700

    Fixing issue with Webpack contantly retrigger watch

--- packages/plugin-webpack/webpack.js
+++ packages/plugin-webpack/webpack.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const webpack = require('webpack');
+const webpackMildCompile = require('webpack-mild-compile');
 const core = require('@theme-tools/core');
 const defaultConfig = require('./config.default');
 const debug = require('debug')('@theme-tools/plugin-webpack');
@@ -61,8 +62,15 @@ module.exports = (userConfig) => {
       })
     );
 
-    webpack(config).watch({
+    // This fixes an issue where WebPack will constantly re-trigger watches for 10s
+    // if the file was changed right before watch was started.
+    // https://github.com/webpack/watchpack/issues/25
+    const compiler = webpack(config);
+    webpackMildCompile(compiler);
+
+    compiler.watch({
       // https://webpack.js.org/configuration/watch/#watchoptions
+      aggregateTimeout: 300,
     }, (err, stats) => {
       if (err) {
         console.error(err.stack || err);
@@ -77,6 +85,7 @@ module.exports = (userConfig) => {
         chunks: false,  // Makes the build much quieter
         colors: true,   // Shows colors in the console
         modules: false, // Hides built modules making output less verbose
+        version: false,
       }));
 
       core.events.emit('reload');
