851b4265eef0b53d1639a518a227716f502c5390
Evan Lovely <evanlovely@gmail.com>
2017-12-15 12:13:18 -0800

    adjust test directory

--- packages/sass-export-data/tests/basics/function.js
+++ packages/sass-export-data/tests/basics/function.js
@@ -2,5 +2,5 @@ const path = require('path');
 const sassExportData = require('../../sass-export-data');
 
 module.exports = sassExportData({
-  path: path.join(__dirname, 'dest/a/b/c'),
+  path: path.join(__dirname, 'dest/sub'),
 });
